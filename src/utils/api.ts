716a425a78ec314b7a5e4f7fea28298e28ee5bf6
Arnaldo Liro <dinholiro@gmail.com>
2025-07-30 09:17:51 -0300

    fix: fixing import error

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -32,39 +32,39 @@ export async function uploadFile(payload: {
 }
 
 
-export async function getFiles(filters: Filters & { page: number; limit?: number}) {
-  const url = new URL("http://localhost:3000/files")
+// export async function getFiles(filters: Filters & { page: number; limit?: number}) {
+//   const url = new URL("http://localhost:3000/files")
 
-  if (filters.search?.trim()) {
-    url.searchParams.append("search", filters.search.trim())
-  }
+//   if (filters.search?.trim()) {
+//     url.searchParams.append("search", filters.search.trim())
+//   }
 
-  if (filters.category && filters.category !== "" && filters.category !== "Todas as categorias") {
-    url.searchParams.append("category", filters.category)
-  }
+//   if (filters.category && filters.category !== "" && filters.category !== "Todas as categorias") {
+//     url.searchParams.append("category", filters.category)
+//   }
 
-  if (filters.date && !isNaN(Date.parse(filters.date))) {
-    url.searchParams.append("date", filters.date)
-  }
+//   if (filters.date && !isNaN(Date.parse(filters.date))) {
+//     url.searchParams.append("date", filters.date)
+//   }
   
-  if (filters.page) {
-    const skip = (filters.page - 1) * (filters.limit || 10)
-    url.searchParams.append("skip", skip.toString())
-  }
+//   if (filters.page) {
+//     const skip = (filters.page - 1) * (filters.limit || 10)
+//     url.searchParams.append("skip", skip.toString())
+//   }
   
-  url.searchParams.append("limit", (filters.limit || 10).toString())
+//   url.searchParams.append("limit", (filters.limit || 10).toString())
 
-  console.log("[getFiles] URL final:", url.toString())
+//   console.log("[getFiles] URL final:", url.toString())
 
-  try {
-    const res = await fetch(url.toString())
-    if (!res.ok) throw new Error("contate a equipe de suporte")
-    return await res.json()
-  } catch (err) {
-    console.error("[getFiles] Erro ao buscar arquivos:", err)
-    throw err
-  }
-}
+//   try {
+//     const res = await fetch(url.toString())
+//     if (!res.ok) throw new Error("contate a equipe de suporte")
+//     return await res.json()
+//   } catch (err) {
+//     console.error("[getFiles] Erro ao buscar arquivos:", err)
+//     throw err
+//   }
+// }
 
 export async function downloadArquivo(id: number): Promise<void> {
   try {
